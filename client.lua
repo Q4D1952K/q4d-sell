8e63bb075a5e76753d8d1a7b3d890332ff66a235
Q4D1952K <97731242+Q4D1952K@users.noreply.github.com>
2022-01-14 17:59:59 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,7 +3,22 @@ PlayerData = {}
 local pedcoords =  Config.Ped
 local soluong = 1
 local NPC
-Citizen.CreateThread(function()
+-- functions
+
+local DrawText3D = function(x, y, z, text)
+	SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    SetDrawOrigin(x,y,z, 0)
+    DrawText(0.0, 0.0)
+    ClearDrawOrigin()
+end
+-- thread
+CreateThread(function()
     while true do
         local sleep =0
         local plyCoords = GetEntityCoords(PlayerPedId(), false)
@@ -15,12 +30,12 @@ Citizen.CreateThread(function()
 				    Wait(10)
 				end
 				TriggerEvent('qg-sell:client:NPC')
-				if dist <= 5 then
-					DrawText3D(pedcoords.x, pedcoords.y, pedcoords.z + 1.02, "~y~DOANH NHÂN")
-		    		DrawText3D(pedcoords.x, pedcoords.y, pedcoords.z + 0.9, "Muốn làm cái giao dịch nho nhỏ nào không?")
-				else
-					sleep = 1500
-                end
+            else
+                sleep = 1500
+            end
+            if dist <= 5 then
+                DrawText3D(pedcoords.x, pedcoords.y, pedcoords.z + 1.02, "~y~DOANH NHÂN")
+                DrawText3D(pedcoords.x, pedcoords.y, pedcoords.z + 0.9, "Muốn làm cái giao dịch nho nhỏ nào không?")
             else
                 sleep = 1500
             end
@@ -30,6 +45,48 @@ Citizen.CreateThread(function()
 		Wait(sleep)
 	end
 end)
+CreateThread(function()
+    while true do
+        local sleep =0
+        local plyCoords = GetEntityCoords(PlayerPedId(), false)
+        local dist = #(plyCoords - vector3(pedcoords.x, pedcoords.y, pedcoords.z))
+            if dist <= 5 then
+                DrawText3D(pedcoords.x, pedcoords.y, pedcoords.z + 1.02, "~y~DOANH NHÂN")
+                DrawText3D(pedcoords.x, pedcoords.y, pedcoords.z + 0.9, "Muốn làm cái giao dịch nho nhỏ nào không?")
+                exports['qb-target']:AddCircleZone("bando", vector3(pedcoords.x, pedcoords.y, pedcoords.z), 2.0, {
+                    name="bando",
+                    debugPoly=false,
+                    useZ=true,
+                    }, {
+                        options = {
+                            {
+                                type = "client",
+                                event = "qg-bando:client:bandomenu",
+                                icon = "fas fa-comment-dollar",
+                                label = "Bán đồ",
+                            },
+                            },
+                        distance = 2.0
+                    })
+            else
+                sleep = 1500
+            end
+        Wait(sleep)
+	end
+end)
+CreateThread(function()
+    local blip1 = AddBlipForCoord(pedcoords.x, pedcoords.y, pedcoords.z)
+    SetBlipSprite(blip1, 58)
+	SetBlipDisplay(blip1, 4)
+	SetBlipScale(blip1, 0.7)
+	SetBlipAsShortRange(blip1, true)
+	SetBlipColour(blip1, 0)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentSubstringPlayerName("Công ty trách nhiệm nhiều thành viên")
+    EndTextCommandSetBlipName(blip1)
+end)
+
+--event
 RegisterNetEvent('qg-sell:client:NPC', function()
     local hash = `a_m_y_business_03`
     NPC = CreatePed(5, hash, vector3(pedcoords.x, pedcoords.y, pedcoords.z - 1), 203.63, false, false)
@@ -39,39 +96,37 @@ RegisterNetEvent('qg-sell:client:NPC', function()
     SetModelAsNoLongerNeeded(hash)
     TaskStartScenarioInPlace(NPC,'WORLD_HUMAN_HANG_OUT_STREET')
 end)
-RegisterNetEvent('qg-bando:client:bandomenu')
-AddEventHandler('qg-bando:client:bandomenu',function()
-        for k, v in pairs(Config.SellItems) do
-            if k == 1 then
-                TriggerEvent('nh-context:sendMenu', {
-                    {
-                    id = 0,
-                    header = "BÁN ĐỒ",
-                    txt = "",
-                    params = {
-                        }
-                    },
-                })
-            end
-            TriggerEvent('nh-context:sendMenu', {
-                {
-                    id = k,
-                    header = QBCore.Shared.Items[v.name].label,
-                    txt = "Giá thu mua: " ..v.banmin.."$ - "..v.banmax.."$",
-                    params = {
-                        event = "qg-bando:client:bando",
-                        args = {
-                            name = v.name,
-                            giamin = v.banmin,
-                            giamax = v.banmax
-                        }
-                    }
-                },
-            })
-        end
+RegisterNetEvent('qg-bando:client:bandomenu' ,function()
+    local Doban = {
+        {
+        header = "BÁN ĐỒ",
+        isMenuHeader = true
+        },
+    }
+    for k, v in pairs(Config.SellItems) do
+        Doban[#Doban+1] = {
+            header = QBCore.Shared.Items[v.name].label,
+            txt = "Giá thu mua: " ..v.banmin.."$ - "..v.banmax.."$",
+            params = {
+            event = "qg-bando:client:bando",
+            args = {
+                name = v.name,
+                giamin = v.banmin,
+                giamax = v.banmax
+                }
+                }
+        }
+    end
+    Doban[#Doban+1] = {
+        header = "X Đóng",
+			params = {
+				event = "qb-menu:client:closeMenu",
+			}
+    }
+    exports['qb-menu']:openMenu(Doban)
 end)
-RegisterNetEvent('qg-bando:client:bando')
-AddEventHandler('qg-bando:client:bando',function(data)
+
+RegisterNetEvent('qg-bando:client:bando' ,function(data)
     local name = data.name
     local giamin = data.giamin
     local giamax = data.giamax
@@ -102,29 +157,3 @@ AddEventHandler('qg-bando:client:bando',function(data)
     end
 end)
 
-Citizen.CreateThread(function()
-    local blip1 = AddBlipForCoord(pedcoords.x, pedcoords.y, pedcoords.z)
-    SetBlipSprite(blip1, 58)
-	SetBlipDisplay(blip1, 4)
-	SetBlipScale(blip1, 0.7)
-	SetBlipAsShortRange(blip1, true)
-	SetBlipColour(blip1, 0)
-	BeginTextCommandSetBlipName("STRING")
-	AddTextComponentSubstringPlayerName("Công ty trách nhiệm nhiều thành viên")
-    EndTextCommandSetBlipName(blip1)
-end)
-
-DrawText3D = function(x, y, z, text)
-	SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(true)
-    AddTextComponentString(text)
-    SetDrawOrigin(x,y,z, 0)
-    DrawText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
-end
